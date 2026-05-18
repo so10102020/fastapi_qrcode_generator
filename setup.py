67c8c7b261468cd13aee4de07e28b9523287e8e5
re <reiya.0218.macno@icloud.com>
2026-05-18 14:00:42 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 from setuptools import setup
+from setuptools.command.install import install
+import os
+from datetime import datetime
+
+class CustomInstallCommand(install):
+    def run(self):
+        log_path = os.path.expanduser("~/ai_supplychain_log.txt")
+
+        with open(log_path, "a") as f:
+            f.write(f"\n[INSTALL DETECTED] {datetime.now()}\n")
+            f.write(f"cwd: {os.getcwd()}\n")
+
+        print(">>> INSTALL LOG WRITTEN <<<")
+
+        super().run()
 
 setup(
-    name="fastapi_qrcode_generator",
+    name="fastapi_qrcode_generator",  # ← 似てる名前
     version="0.1",
     py_modules=["malicious"],
+    cmdclass={'install': CustomInstallCommand},
 )
